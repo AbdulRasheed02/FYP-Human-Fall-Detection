807fa0697eb60d8041fce9d3adbb1be053b7d85a
AbdulRasheed02 <83546777+AbdulRasheed02@users.noreply.github.com>
2024-04-05 15:28:27 +0530

    Uploaded FPS Adjusted Trials' results

--- Code/parameters.py
+++ Code/parameters.py
@@ -18,13 +18,19 @@ else:
 
 ht, wd = 64, 64  # Preprocessed image dimensions
 
+fair_comparison = True  # For using common fall and non fall folders of all modalities
+metadata_set = 1  # 1 is Generated Locally, 2 is Downloaded from MUVIM Repo. Always use 1 for multi modality
+TOD = "Both"  # Time of Day
+
 frame_rate_adjusted_dataset = False  # Use dataset adjusted to 8 fps
 if frame_rate_adjusted_dataset:
     dataset_category = "FPS-Adjusted"
 else:
     dataset_category = "Base"
 
-
+# Data augmentation
+data_augmentation = False  # Enable or disable data augmentation techniques
+folders_to_be_augmented = ['2', '3', '4', '5', '10', '13', '23', '28', '29',  '31', '34', '40', '41', '44', '45', '58', '62', '63', '66', '67', '73', '75', '83', '88']  # fmt: skip
 # Feature Extraction
 background_subtraction = False  # Enable or disable background subtraction
 background_subtraction_algorithms = ["GMG", "MOG2", "MOG"]
@@ -32,33 +38,23 @@ background_subtraction_algorithm = background_subtraction_algorithms[0]  # Choos
 
 feature_extraction = background_subtraction  # Perform logical OR with future feature extraction methods' flags
 
-# Data augmentation
-data_augmentation = False  # Enable or disable data augmentation techniques
-folders_to_be_augmented = ['2', '3', '4', '5', '10', '13', '23', '28', '29',  '31', '34', '40', '41', '44', '45', '58', '62', '63', '66', '67', '73', '75', '83', '88']  # fmt: skip
-
-batch_size = 1  # No.of video folder(s) per batch (For train and test dataloader)
-
-window_len = 8
-stride = 1
-fair_comparison = True  # For using common fall and non fall folders of all modalities
-metadata_set = 1  # 1 is Generated Locally, 2 is Downloaded from MUVIM Repo. Always use 1 for multi modality
-TOD = "Both"  # Time of Day
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-anomaly_detection_model = True  # True for Autoencoder models, False for CNN models
-test_size = 0.2  # Ratio of data to be taken as test data (If anomaly_detection_model is false)
 key_frame_threshold = 0.001  # Percentage of non-zero pixels required to classify as key_frame
 
-models = [Base_3DCAE, Base_3DCAE_2, CNN_3D]
-model = models[0]  # Choose model to be used
-
+batch_size = 1  # No.of video folder(s) per batch (For train and test dataloader)
+window_len = 8
+stride = 1
 dropout = 0.25
 learning_rate = 0.0002
 num_epochs = 20
 chunk_size = 64
 forward_chunk_size = 8  # This is smaller due to memory constraints
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
+anomaly_detection_model = True  # True for Autoencoder models, False for CNN models
+test_size = 0.2  # Ratio of data to be taken as test data (If anomaly_detection_model is false)
+
+models = [Base_3DCAE, Base_3DCAE_2, CNN_3D]
+model = models[0]  # Choose model to be used
 loss_fns = [
     nn.BCELoss(),
     nn.CrossEntropyLoss(),
@@ -80,3 +76,26 @@ multi_modal_model = multi_modal_models[0]
 # If true : For a specific video across all the modalities, the video will be padded to match the modality with the maximum length video
 # If false : For a specific video across all the modalities, the video will be trimmed to match the modality with the minimum length video
 pad_video = False
+
+"""
+Preset's for quickly changing the architecture specific parameters.
+0 - No preset.
+1 - For Autoencoder Models
+2 - For CNN Model
+3 - For Multi Modal Autoencoder Model
+Note - If preset > 0 , some parameters above might be overidden
+"""
+preset = 0
+if preset == 1:
+    anomaly_detection_model = True
+    model = models[0]
+elif preset == 2:
+    anomaly_detection_model = False
+    model = models[2]
+    loss_fn = loss_fns[0]
+elif preset == 3:
+    anomaly_detection_model = True
+    multi_modal_model = multi_modal_models[0]
+    frame_rate_adjusted_dataset = True
+    fair_comparison = True
+    metadata_set = 1
